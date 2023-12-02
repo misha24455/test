757fc7ec4ad18828e1f740e30686ff7a7aa216b4
misha24455 <svimon1309@gmail.com>
2023-12-02 17:46:13 +0300

    eee

---
+++ script.js
@@ -0,0 +1,23 @@
+class Product {
+    imageLink; 
+    productName;
+    ProductPrise;
+
+    constructor(link,name,prise){
+        this.imageLink = link;
+        this.productName = name;
+        this.ProductPrise = prise;
+        card.className = "card";
+        let card = document.createElement('div');
+        card.innerHTML =
+        `<img src="${this.imageLink}">
+        <h3>${this.productName}</h3>
+        <h4>${this.ProductPrise}</h4>
+        <button>Купить</button>
+        `
+        document.body.append(card);
+    }
+
+}
+
+new Product('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-mIFOhjgO7j--mFJnC4idDrSFWV7Vzm_uo06fHkA&s','nmae','199');
